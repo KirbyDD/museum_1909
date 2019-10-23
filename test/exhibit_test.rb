9b2c8bd6ee29f2826ea421eeb0aba76b419e1f0a
DeMarcus Kirby <tkd024@gmail.com>
2019-10-23 09:13:43 -0600

    Add Exhibit test suite

---
+++ test/exhibit_test.rb
@@ -0,0 +1,24 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/exhibit.rb'
+
+class ExhibitTest < Minitest::Test 
+  
+  def setup
+    @exhibit = Exhibit.new("Gems and Minerals", 0)
+  end
+
+  def test_it_exist
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_has_a_name
+    assert_equal "Gems and Minerals", @exhibit.name
+  end
+
+  def test_it_has_a_price
+    assert_equal 0, @exhibit.price
+  end
+
+end
