c1b852fe55710ff3bbf337f78e8d72da691a5da0
DeMarcus Kirby <tkd024@gmail.com>
2019-10-23 10:13:03 -0600

    Update Museum Test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -55,4 +55,37 @@ class MuseumTest < Minitest::Test
     assert_equal [imax], @dmns.recommend_exhibits(sally)
   end
 
+  def test_it_has_no_patrons_by_default
+    assert_equal [] @dmns.patrons
+  end
+
+  def test_admit_method
+    bob = Patron.new("Bob", 20)
+    @dmns.admit(bob)
+    assert_equal [bob] @dmns.patrons
+  end
+
+  def test_patrons_by_exhibit_interest_method
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    imax = Exhibit.new("IMAX", 15)
+
+    bob = Patron.new("Bob", 20)
+    sally = Patron.new("Sally", 20)
+
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally.add_interest("Dead Sea Scrolls")
+
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+
+    @dmns.admit(bob)
+    @dmns.admit(sally)
+
+    expected = {"Dead Sea Scrolls" => [bob,sally], "Gems and Minerals" => [bob], "IMAX" => []}
+    assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
+
 end
